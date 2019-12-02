a69c120203da796298311eb6e9d4e9f2aec842e7
tosu1010 <ikiblea10@gmail.com>
2019-12-02 20:59:50 +0900

    レビュー投稿機能の作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,10 +8,23 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    
+    artist = Artist.find_artist(review_params[:artist])
+    album = Album.create_album(review_params[:album], artist.id)
+    Review.create(
+      content: review_params[:review],
+      user_id: current_user.id,
+      album_id: album.id,
+      artist_id: artist.id
+    )
   end
 
   def show
     
+  end
+
+
+  private
+  def review_params
+    params.permit(:album, :artist, :review)
   end
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,7 @@
 class Album < ApplicationRecord
   belongs_to :artist
+
+  def self.create_album(artist_id, title)
+    album = Album.create(title: title, artist_id: artist_id)
+  end
 end
