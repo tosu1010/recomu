286cf9098b105f60db64e8f41c026661aa418e1f
tosu1010 <ikiblea10@gmail.com>
2019-12-15 20:44:58 +0900

    いいね機能の実装
    レビューを表示する画面にていいねを押すことができるように実装

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,12 @@ class Review < ApplicationRecord
   has_many :reviews_tags
   has_many :tags, through: :reviews_tags
   has_many :comments
+  has_many :likes
   belongs_to :user
   belongs_to :album
+
+  # 当該レビューに対して、引数に渡したユーザーがいいねを押しているかの判断
+  def liked_by(user_id)
+    likes.where(user_id: user_id).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :reviews, only: [:index, :new, :create, :show] do
     resources :comments, only: [:create, :new]
+    resources :likes, only: [:create, :destroy]
   end
   root "reviews#index"
 end
