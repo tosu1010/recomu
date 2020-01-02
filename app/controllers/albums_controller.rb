755413cfdefc88a947948303e246a0941d3f0a59
tosu1010 <ikiblea10@gmail.com>
2020-01-03 01:23:23 +0900

    album/showのviewに試聴フォームを表示

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,12 @@
 class AlbumsController < ApplicationController
+  include SpotifyMethod
+
   def index
     @albums = Album.all
   end
 
   def show
     @album = Album.find(params[:id])
-    @reviews = @album.reviews
-    @another_albums = @album.another_albums()
+    @tracks = @album.spotify_id ? get_tracks(@album.spotify_id) : nil
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  include SpotifyMethod
+
   def index
     
   end
@@ -43,42 +45,5 @@ class ReviewsController < ApplicationController
   private
   def review_params
     params.permit(:album, :artist, :review, tags: [])
-  end
-
-  def spotify_auth
-    client_id = Rails.application.credentials.spotify[:client_id]
-    client_secret = Rails.application.credentials.spotify[:client_secret]
-    RSpotify.authenticate(client_id, client_secret)
-  end
-
-  # artistのspotify_idを取得する
-  def get_artist_spotify_id(artist_name)
-    spotify_auth
-
-    artist = RSpotify::Artist.search(artist_name).first
-    artist.present? ? artist.id : nil
-  end
-
-  # 引数に渡したalbumのspotify_idを取得する
-  def get_album_spotify_id(artist_spotify_id, album_name)
-    spotify_auth
-
-    artist = RSpotify::Artist.find(artist_spotify_id)
-    album = artist.albums(album_type: 'single', limit: 50).find { |album| album.name == album_name }
-    unless album
-      album = artist.albums(album_type: 'album', limit: 50).find { |album| album.name == album_name }
-      unless album
-        album = artist.albums(album_type: 'compilation', limit: 50).find { |album| album.name == album_name }
-      end
-    end
-    album ? album.id : nil
-  end
-
-  # アルバムの画像を探す
-  def get_album_image(album_spotify_id)
-    spotify_auth
-
-    image = RSpotify::Album.find(album_spotify_id)
-    image ? image.images[0]['url'] : 'no_image.png'
   end
 end
