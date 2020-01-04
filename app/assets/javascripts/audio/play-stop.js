ca72a459ab833aad64588fbd6004c5e52e4ac6d1
tosu1010 <ikiblea10@gmail.com>
2020-01-04 15:28:13 +0900

    再生・停止処理
    play-stop.jsにて試聴音源の再生、停止処理の作成

---
+++ app/assets/javascripts/audio/play-stop.js
@@ -0,0 +1,25 @@
+$(function(){
+  // 再生処理
+  $(document).on('click', '.track__audio__button', function() {
+    let audio = $(this).siblings('.prev-audio')[0]
+    audio.play();
+    $(this).hide()
+    $(this).siblings('.track__audio__button--stop').show();
+
+    // 再生終了時のリセット処理
+    audio.addEventListener('ended', function(){
+      console.log('30秒');
+      audio.pause();
+      audio.currentTime = 0;
+      $(this).siblings('.track__audio__button--stop').hide();
+      $(this).siblings('.track__audio__button').show();
+    }, false)
+  });
+
+  // 一時停止処理
+  $(document).on('click', '.track__audio__button--stop', function() {
+    $(this).siblings('.prev-audio')[0].pause();
+    $(this).hide()
+    $(this).siblings('.track__audio__button').show();
+  });
+});
