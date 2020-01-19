22b35e2aa2a37784f8e0265c49989411a9474aab
tosu1010 <ikiblea10@gmail.com>
2020-01-19 14:11:26 +0900

    sign_upのスタイルの適用

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,18 @@ class CommentsController < ApplicationController
   before_action :set_review, only: [:create]
 
   def create
-    @comment = @review.comments.new(comment_params)
-    if @comment.save
-      respond_to do |format|
-        format.json
-        format.html { redirect_to review_path(@review) }
+    if user_signed_in?
+      @comment = @review.comments.new(comment_params)
+      if @comment.save
+        respond_to do |format|
+          format.json
+          format.html { redirect_to review_path(@review) }
+        end
+      else
+        flash[:alert] = '文字を入力してください'
       end
     else
-      flash[:alert] = '文字を入力してください'
+      redirect_to new_user_session_path
     end
   end
 
