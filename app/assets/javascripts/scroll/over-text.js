a127b79d18a4bff23daab6ce2d4c76bf91a3fcd0
tosu1010 <ikiblea10@gmail.com>
2020-01-22 12:45:01 +0900

    スマホ用のスタイルの適用

--- app/assets/javascripts/scroll/over-text.js
+++ app/assets/javascripts/scroll/over-text.js
@@ -2,8 +2,14 @@ $(document).on('turbolinks:load', function(){
   $(function(){
     $('.over-scroll').each(function(){
       let text = $(this).text().length;
-      if(text > 11) {
-        $(this).addClass('over-scroll--scroll');
+      if($(window).width() > 768) {
+        if(text > 11) {
+          $(this).addClass('over-scroll--scroll');
+        }
+      } else {
+        if(text > 6) {
+          $(this).addClass('over-scroll--scroll');
+        }
       }
     });
   });
